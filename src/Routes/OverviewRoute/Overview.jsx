2f43f7031b409c613cadc77626c2f157f1f5befc
Khilaar <luca.oliverio@hotmail.com>
2023-11-11 01:25:41 +0100

    now I fetched the products in the overview route

--- src/Routes/OverviewRoute/Overview.jsx
+++ src/Routes/OverviewRoute/Overview.jsx
@@ -1,7 +1,38 @@
+import { useEffect, useState } from "react";
+
 const Overview = () => {
+    const [allProducts, setAllProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('https://dummyjson.com/products');
+                const data = await response.json();
+                setAllProducts(data);
+                setLoading(false);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return (
         <div>
-            <h2>Overview Page</h2>
+            {loading ? (
+                <p>Loading...</p>
+            ) : (
+                allProducts && allProducts.products && allProducts.products.length > 0 ? (
+                    allProducts.products.map((product) => (
+                        <p key={product.id}>{product.title}</p>
+                    ))
+                ) : (
+                    <p>No products found.</p>
+                )
+            )}
         </div>
     );
 };
